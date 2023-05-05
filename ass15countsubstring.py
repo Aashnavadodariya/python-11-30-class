0e81abd01dc561ce793a9fc637756ab990ac51c0
Aashnavadodariya <aashuvadodariya@gmail.com>
2023-05-04 22:39:56 -0600

    5-may-2023

---
+++ ass15countsubstring.py
@@ -0,0 +1,7 @@
+#write a python program to count occurence of a substring in a string
+my_string=input("enter a string :")
+my_substring=input("enter a substring :")
+
+
+my_count = my_string.count(my_substring)
+print("substring occurs %d times" %my_counts)
